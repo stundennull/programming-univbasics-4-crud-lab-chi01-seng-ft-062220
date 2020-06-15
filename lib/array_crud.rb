241084e15181644a5c5416517ffb028b90a5d34a
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-15 02:48:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,19 @@
 def create_an_empty_array
-  
+  empty_array = Array.new
 end
 
 def create_an_array
-  
+  waveforms = ["Sawtooth", "Sine", "Square", "Pulse"] 
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  waveforms = ["Sawtooth", "Sine", "Square", "Pulse"]
+  waveforms << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  waveforms = ["Sawtooth", "Sine", "Square", "Pulse"]
+  waveforms.unshift = "wow"
 end
 
 def remove_element_from_end_of_array(array)
